bffa6c643f4add159d3010037f170218ec190296
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-17 12:43:32 +0300

    Migrations

--- NLayerArchitecture.API/Program.cs
+++ NLayerArchitecture.API/Program.cs
@@ -1,3 +1,12 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using NLayerArchitecture.Core.Repositories;
+using NLayerArchitecture.Core.Services;
+using NLayerArchitecture.Core.UnitOfWorks;
+using NLayerArchitecture.Repository;
+using NLayerArchitecture.Repository.Repositories;
+using NLayerArchitecture.Repository.UnitOfWorks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), options =>
+    {
+        options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
 
 var app = builder.Build();
 
--- NLayerArchitecture.Repository/UnitOfWorks/UnitOfWork.cs
+++ NLayerArchitecture.Repository/UnitOfWorks/UnitOfWork.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLayerArchitecture.Core.UnitOfWorks;
 
 namespace NLayerArchitecture.Repository.UnitOfWorks
 {
-    internal class UnitOfWork
+    public class UnitOfWork:IUnitOfWork
     {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
     }
 }
