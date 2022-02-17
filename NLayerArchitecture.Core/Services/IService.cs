03a59a22493f8a3f83a147a17169f5becaa3de96
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-17 11:14:40 +0300

    IUnitOfWork

--- NLayerArchitecture.Core/Services/IService.cs
+++ NLayerArchitecture.Core/Services/IService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace NLayerArchitecture.Core.Services
 {
-    internal interface IService
+    public interface IService<T> where T:class
     {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        // productrepository.where(x=>x.id>5).orderBy().ToListAsync()
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity); // asenkron metodu yok
+
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
     }
 }
