3863d31cd9946014f22f556d27b6f9d69b4383a3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-17 11:00:56 +0300

    IGenericRepository

---
+++ NLayerArchitecture.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayerArchitecture.Core.Repositories
+{
+    public interface IGenericRepository<T> where T:class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        // productrepository.where(x=>x.id>5).orderBy().ToListAsync()
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity); // asenkron metodu yok
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
