6f081e5f525610d739e9e81897649b2e1886c6cf
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-18 10:26:55 +0300

    CustomResponseDto

---
+++ NLayerArchitecture.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayerArchitecture.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore] // kod içinde lazım ama dönüş tipinde görülmücek
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T>() {Data = data, StatusCode = statusCode};
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T>() {  StatusCode = statusCode };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<String> errors)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode ,Errors = errors};
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode, Errors = new List<string>{error}};
+        }
+    }
+
+}
