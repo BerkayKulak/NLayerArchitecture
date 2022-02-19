761ac2b090bef193bbca1b8bca14d69c340e3bc2
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-19 11:40:00 +0300

    NotFoundFilter

---
+++ NLayerArchitecture.API/Filters/NotFoundFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerArchitecture.Core.DTOs;
+using NLayerArchitecture.Core.Model;
+using NLayerArchitecture.Core.Services;
+
+namespace NLayerArchitecture.API.Filters
+{
+    public class NotFoundFilter<T> : IAsyncActionFilter where T : BaseEntity
+    {
+
+        private readonly IService<T> _service;
+
+        public NotFoundFilter(IService<T> service)
+        {
+            _service = service;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            var idValue = context.ActionArguments.Values.FirstOrDefault();
+
+            if (idValue == null)
+            {
+                await next.Invoke();
+                return;
+            }
+
+            var id = (int)idValue;
+            var anyEntity = await _service.AnyAsync(x => x.Id == id);
+
+            if (anyEntity)
+            {
+                await next.Invoke();
+                return;
+            }
+
+            context.Result = new NotFoundObjectResult(CustomResponseDto<NoContentDto>.Fail(404, $"{typeof(T).Name}({id}) not found"));
+
+        }
+    }
+}
